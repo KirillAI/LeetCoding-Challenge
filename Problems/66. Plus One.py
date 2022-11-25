e185265225b7219c734d121d2011c59960eef686
Kirill Akhmetzyanov <kirill.akhmetzyanov@gmail.com>
2022-11-25 19:40:43 +0700

    Add solution for Plus One

---
+++ Problems/66. Plus One.py
@@ -0,0 +1,72 @@
+'''
+Plus One
+https://leetcode.com/problems/plus-one/
+#Array #Easy
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+Example 1:
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Example 2:
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Example 3:
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+
+Constraints:
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+'''
+
+#**********************************
+#*********** VERSION 1 ************
+#**********************************
+
+'''
+Time: O(N)
+Memory: O(N)
+'''
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        return list(map(int, [*str(int(''.join(list(map(str, digits)))) + 1)]))
+
+#**********************************
+#*********** VERSION 2 ************
+#**********************************
+
+'''
+Time: O(N)
+Memory: O(N)
+'''
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+        for i in range(len(digits) - 1, 0, -1):
+            d = digits[i]
+            if d == 10:
+                digits[i - 1] += 1
+                digits[i] = 0
+            else:
+                break
+        if digits[0] == 10:
+            digits[0] = 0
+            digits.insert(0, 1)
+        return digits
